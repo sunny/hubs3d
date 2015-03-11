9541cbec1afbd760d5fb7d4763f0af0d3fa9492e
Sunny Ripert <sunny@sunfox.org>
2015-03-11 13:05:23 +0100

    Cart spec

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -2,6 +2,36 @@ require "spec_helper"
 require "hubs3d/cart"
 
 describe Hubs3D::Cart do
-  # pending "#<<"
-  # pending "#url"
+  let(:cart) { Hubs3D::Cart.new }
+  let(:item) { Minitest::Mock.new }
+
+  describe "#items" do
+    it "defaults to empty" do
+      cart.items.empty?.must_equal true
+    end
+  end
+
+  describe "#<<" do
+    it "adds items" do
+      cart << item
+      cart << item
+      cart.items.size.must_equal 2
+    end
+  end
+
+  describe "#url" do
+    before do
+      cart << item
+      item.expect :id, 42
+      item.expect :id, 42
+    end
+
+    it "calls the API" do
+      # params = { items: { modelId: 42, quantity: 1 } }
+      returned = { url: "http://example" }
+      Hubs3D::API.stub :post, "/cart", returned do
+        cart.url.must_equal "http://example"
+      end
+    end
+  end
 end
